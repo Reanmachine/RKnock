fc70b02329b9bc86af8496de8f5dcc39fd2828ec
unknown <ReAn@.Reanmachine.local>
2009-09-11 16:42:51 -0600

    - About Dialog

---
+++ aboutdialog.cpp
@@ -0,0 +1,42 @@
+#include "core.h"
+#include "aboutdialog.h"
+#include "ui_aboutdialog.h"
+
+#include <QtGlobal>
+#include <QApplication>
+
+AboutDialog::AboutDialog(QWidget *parent) :
+    QDialog(parent),
+    m_ui(new Ui::AboutDialog)
+{
+    m_ui->setupUi(this);
+
+    this->setAttribute(Qt::WA_DeleteOnClose, true); // Delete me when I close!
+    this->setAttribute(Qt::WA_QuitOnClose, false);
+
+    this->m_ui->lblQtVersion->setText(QString(qVersion()));
+    this->m_ui->lblApplicationLine->setText(QString(RKNOCK_VERSION));
+
+    QApplication *app = (QApplication*)QApplication::instance();
+    QString line = QString("%1 %2").arg(app->organizationName(), app->applicationName());
+    this->m_ui->lblApplicationLine->setText(line);
+
+    this->setWindowTitle(QString("About %1").arg(app->applicationName()));
+}
+
+AboutDialog::~AboutDialog()
+{
+    delete m_ui;
+}
+
+void AboutDialog::changeEvent(QEvent *e)
+{
+    QDialog::changeEvent(e);
+    switch (e->type()) {
+    case QEvent::LanguageChange:
+        m_ui->retranslateUi(this);
+        break;
+    default:
+        break;
+    }
+}
---
+++ core.h
@@ -0,0 +1,7 @@
+#ifndef CORE_H
+#define CORE_H
+
+#define RKNOCK_VERSION          "1.0.1"
+#define RKNOCK_CFG_VERSION      "1.0.0"
+
+#endif // CORE_H
